53b53a3e4277ba54866ffa1cf81a1539a7e6dd97
Harshit Singh <harshitsingh05893312@gmail.com>
2025-09-23 22:32:12 +0530

    Update mainn.py

--- mainn.py
+++ mainn.py
@@ -1,8 +1,27 @@
 import json
-from fastapi import FastAPI
+from fastapi import FastAPI, Path, HTTPException, Query
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
 
 app = FastAPI()
 
+class Patient(BaseModel):
+    
+    id: Annotated[str, Field(..., description= 'ID of the Patient', examples=['P001'])]
+    name : Annotated[str, Field(..., description= 'Name of the patient')]
+    city : Annotated[str, Field(..., description= 'City where the patient live')]
+    age : Annotated[int, Field(..., gt =0, lt = 120, description='Age of the patient')]
+    gender : Annotated[Literal['male', 'female', 'other'], Field(..., description =  'Gender of the patient')]
+    height : Annotated[float, Field(..., gt = 0, description= 'Hieght of the patient in mtrs')]
+    weight : Annotated[float, Field(..., gt = 0, description= 'Weight of the patient in kg')]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height)**2, 2)   # rounding off bmi to two digits
+        return bmi
+    
+    
 def load_data():
     with open('patients.json', 'r') as f:
         data = json.load(f)
@@ -22,6 +41,7 @@ def about():
 def view_patients():
     data = load_data()
     return {'data': data}
+
 # PATH PARAMETER
 
 @app.get('/view_patient/{patient_id}')
@@ -55,5 +75,7 @@ def sort_patients(sort_by: str = Query(..., description='Sort on the basis of he
 
     
     return sorted_data
+ 
+
 
 
